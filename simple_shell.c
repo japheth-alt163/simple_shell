f8c824e8c83952bd537e284c5cb76ae9b7bb97d3
kaysee clintone <kayseeclintone@gmail.com>
2023-09-20 01:23:22 +0300

    Update simple_shell.c

--- simple_shell.c
+++ simple_shell.c
@@ -1,60 +1,48 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/wait.h>
-
 #define MAX_INPUT_LENGTH 1024
 
-int main(void)
-{
+int main(void) {
 	pid_t pid = fork();
+    char input[MAX_INPUT_LENGTH];
 
-	char input[MAX_INPUT_LENGTH];
-	while (1)
-	{
-	/* Display the prompt */
-	printf("#cisfun$ ");
-	fflush(stdout);
-	/* Read a line of input */
-	if (fgets(input, sizeof(input), stdin) == NULL)
-	{
-		/* Handle Ctrl+D (end of file) */
-		printf("\n");
-		break;
-	}
-	/* Remove the newline character at the end */
-	input[strcspn(input, "\n")] = '\0';
-	/* Fork a new process */
-	if (pid == -1)
-	{
-		perror("fork");
-		 exit(EXIT_FAILURE);
-	}
-	else if (pid == 0)
-	{
-		/* Child process */
-		/* Execute the command */
-		if (execlp(input, input, (char *)NULL) == -1)
-		{
-		/* Handle the error if the command couldn't be executed */
-		perror("execlp");
-		exit(EXIT_FAILURE);
-		}
-	}
-	else
-	{
-		/* Parent process */
-		/* Wait for the child to finish */
-		int status;
+    while (1) {
+        /* Display the prompt */
+        printf("#cisfun$ ");
+        fflush(stdout);
 
-		if (waitpid(pid, &status, 0) == -1)
-		{
-		perror("waitpid");
-		exit(EXIT_FAILURE);
-		}
-	}
-	}
-	return (0);
-}
+        /* Read a line of input */
+        if (fgets(input, sizeof(input), stdin) == NULL) {
+            /* Handle Ctrl+D (end of file) */
+            printf("\n");
+            break;
+        }
+
+        /* Remove the newline character at the end */
+        input[strcspn(input, "\n")] = '\0';
+
+        /* Fork a new process */
+
+        if (pid == -1) {
+            perror("fork");
+            exit(EXIT_FAILURE);
+        } else if (pid == 0) {
+            /* Child process */
+
+            /* Execute the command */
+            if (execlp(input, input, (char *)NULL) == -1) {
+                /* Handle the error if the command couldn't be executed */
+                perror("execlp");
+                exit(EXIT_FAILURE);
+            }
+        } else {
+            /* Parent process */
+
+            /* Wait for the child to finish */
+            int status;
+            if (waitpid(pid, &status, 0) == -1) {
+                perror("waitpid");
+                exit(EXIT_FAILURE);
+            }
+        }
+    }
+
+    return 0;
