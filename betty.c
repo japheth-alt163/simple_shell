5434c87b842ebc022ba861e7eff5bf4f4e4b05a9
kaysee clintone <kayseeclintone@gmail.com>
2023-09-20 23:12:48 +0300

    fixed betty issues in betty.c

--- betty.c
+++ betty.c
@@ -1,31 +1,31 @@
 #include "shell.h"
 /**
- * betty_main - Entry point
+ * main - Entry point
  *
  * Description: This program calculates the sum of two integers and
  *              prints a message based on the value of the sum.
  *
  * Return: Always 0 (Success)
  */
-int betty_main(void)
+int main(void)
 {
 int a, b, c;
 
 printf("Enter the values of a and b:\n");
 scanf("%d %d", &a, &b);
 c = a + b;
-printf("Sum is %d", c);
+printf("Sum is %d\n", c);
 if (c > 20)
 {
-printf("\nSum is greater than 20");
+printf("Sum is greater than 20\n");
 }
 else if (c > 15)
 {
-printf("\nSum is greater than 15");
+printf("Sum is greater than 15\n");
 }
 else if (c > 10)
 {
-printf("\nSum is less than 10");
+printf("Sum is less than 10\n");
 }
 return (0);
 }
