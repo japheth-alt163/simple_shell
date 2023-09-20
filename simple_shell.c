ed63e153015f670cce25cd2dd8356bb3150588ef
kaysee clintone <kayseeclintone@gmail.com>
2023-09-21 00:08:07 +0300

    Update simple_shell.c

--- simple_shell.c
+++ simple_shell.c
@@ -1,56 +1,68 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/wait.h>
+#include "shell.h"
 
-#define MAX_INPUT_LENGTH 1024
+#define MAX_INPUT_SIZE 1024
 
-int main(void) {
-	 pid_t pid = fork();
-    char input[MAX_INPUT_LENGTH];
+/**
+ * display_prompt - displays the comand- line prompt
+*/
+void display_prompt(void)
+{
+printf("#cisfun$ ");
+fflush(stdout);
+}
 
-    while (1) {
-        /* Display the prompt */
-        printf("#cisfun$ ");
-        fflush(stdout);
+/**
+ * execute_command - tries to verify the command passed to it
+ * and execute it
+ * @command: the command param
+*/
+void execute_command(const char *command)
+{
+pid_t child_pid = fork();
+int status;
 
-        /* Read a line of input */
-        if (fgets(input, sizeof(input), stdin) == NULL) {
-            /* Handle Ctrl+D (end of file) */
-            printf("\n");
-            break;
-        }
-
-        /* Remove the newline character at the end */
-        input[strcspn(input, "\n")] = '\0';
-
-        /* Fork a new process */
-        
-        if (pid == -1) {
-            perror("fork");
-            exit(EXIT_FAILURE);
-        } else if (pid == 0) {
-            /* Child process */
-
-            /* Execute the command */
-            if (execlp(input, input, (char *)NULL) == -1) {
-                /* Handle the error if the command couldn't be executed */
-                perror("execlp");
-                exit(EXIT_FAILURE);
-            }
-        } else {
-            /* Parent process */
-
-            /* Wait for the child to finish */
-            int status;
-            if (waitpid(pid, &status, 0) == -1) {
-                perror("waitpid");
-                exit(EXIT_FAILURE);
-            }
-        }
-    }
-
-    return 0;
+if (child_pid == -1)
+{
+perror("fork");
+exit(EXIT_FAILURE);
+}
+else if (child_pid == 0)
+{
+if (execlp(command, command, NULL) == -1)
+{
+perror("./shell");
+exit(EXIT_FAILURE);
+}
+}
+else
+{
+if (wait(&status) == -1)
+{
+perror("wait");
+}
+}
+}
+/**
+ * main - Entry point of the shell program
+ * Return: command_line (True)
+*/
+int main(void)
+{
+char input[MAX_INPUT_SIZE];
+while (1)
+{
+display_prompt();
+if (fgets(input, sizeof(input), stdin) == NULL)
+{
+printf("\n");
+break;
+}
+input[strcspn(input, "\n")] = '\0';
+if (strcmp(input, "exit") == 0)
+{
+break;
+}
+execute_command(input);
+}
+return (EXIT_SUCCESS);
 }
